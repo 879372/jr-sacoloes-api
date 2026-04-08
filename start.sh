8ed3a779a73060645fd90d60569321c5dfa2f2a3
879372 <kaios5028@gmail.com>
2026-04-08 11:49:09 -0300

    deploy: add requirements.txt and fix start.sh for production

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Rodar migrações automaticamente no deploy
-python manage.py migrate
-# Iniciar o servidor WSGI com Gunicorn
-gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
+# Rodar migrações automaticamente no deploy usando python3
+python3 manage.py migrate
+# Iniciar o servidor WSGI com Gunicorn usando o caminho do binário ou módulo
+python3 -m gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
